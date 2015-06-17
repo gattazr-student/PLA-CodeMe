55566e585ee50181e278f1e7affee660247bac70
gattazr <remi.gattaz@gmail.com>
2015-06-17 11:42:56 +0200

    Ajout de CaseVide et CaseInterrupteur

--- lightbot_2.0/src/models/action/Allumer.java
+++ lightbot_2.0/src/models/action/Allumer.java
@@ -3,6 +3,7 @@ package models.action;
 import models.bot.Bot;
 import models.niveau.Carte;
 import models.niveau.Case;
+import models.niveau.CaseInterrupteur;
 import models.niveau.CaseLampe;
 
 /**
@@ -18,12 +19,18 @@ public class Allumer extends Action {
 		if (aCase instanceof CaseLampe) {
 			((CaseLampe) aCase).activate();
 		}
+		if (aCase instanceof CaseInterrupteur) {
+			((CaseInterrupteur) aCase).changeCase(aCarte);
+		}
 	}
 
 	@Override
 	public boolean valid(Bot aBot, Carte aCarte) {
 		Case aCase = aCarte.getCase(aBot.getPosition());
 		if (aCase instanceof CaseLampe) {
+			return true;
+		}
+		if (aCase instanceof CaseInterrupteur) {
 			return true;
 		}
 		return false;
