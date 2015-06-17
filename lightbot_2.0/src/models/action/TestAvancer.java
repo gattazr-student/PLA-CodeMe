90c058eab7a1b5ed35af25aa3a45f56b0ed0d52a
SaraOunissi <sara.ounissi@gmail.com>
2015-06-17 09:45:52 +0200

    Action Test Avancer et Test Sauter

--- lightbot_2.0/src/models/action/TestAvancer.java
+++ lightbot_2.0/src/models/action/TestAvancer.java
@@ -1,10 +1,8 @@
 package models.action;
 
 import models.basic.Couleur;
-import models.basic.Position;
 import models.bot.Bot;
 import models.niveau.Carte;
-import models.niveau.Case;
 
 public class TestAvancer extends Action {
 
@@ -12,41 +10,13 @@ public class TestAvancer extends Action {
 
 	@Override
 	public void apply(Bot aBot, Carte aCarte) {
-		/*
-		 * Met le robot en Vert si on peut avancer (dans le terrain, sauf trou)
-		 * Sinon le robot devient Rouge
-		 */
-		/*
-		 * Acion valide si pas de case (segfault) ou si case destination sur même niveau
-		 */
-		Case wCaseCourant, wCaseDestination;
-		Position wPositionCourant, wNextPosition;
+		Action aAvancer = new Avancer();
 
-		wPositionCourant = aBot.getPosition();
-		wNextPosition = wPositionCourant.move(aBot.getOrientation());
-
-		wCaseCourant = aCarte.getCase(wPositionCourant);
-		wCaseDestination = aCarte.getCase(wNextPosition);
-
-		/*
-		 * Si la case destination est null, le déplacement est interdit
-		 */
-		if (wCaseDestination == null) {
+		if (aAvancer.valid(aBot, aCarte)) {
+			aBot.setCouleur(Couleur.VERT);
+		} else {
 			aBot.setCouleur(Couleur.ROUGE);
 		}
-
-		int wDestinationHauteur = wCaseDestination.getHauteur();
-		int wCouranthauteur = wCaseCourant.getHauteur();
-		int wDiff = wDestinationHauteur - wCouranthauteur;
-
-		/* La différence doit être de 0 */
-		if (wDiff == 0) {
-			aBot.setCouleur(Couleur.VERT);
-		}
-
-		/* La destination et le courant ne sont pas sur le même niveau */
-		aBot.setCouleur(Couleur.ROUGE);
-		;
 	}
 
 	@Override
